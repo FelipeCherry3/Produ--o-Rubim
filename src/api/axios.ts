bf9a407f264f9744bac749c79a99082e4ecdcb3b
FelipeCherry3 <felipeelias609@hotmail.com>
2025-11-06 09:28:39 -0300

    error-network

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
-
-const baseUrl = import.meta.env.VITE_BACK_URL ?? '/'; // Em dev pode usar proxy ('/') ou setar BACK_URL
+// Em dev pode usar proxy ('/') ou setar BACK_URL
 
 const api = axios.create({
   baseURL: "https://pcpbackend-production.up.railway.app/",        // usa variável de ambiente BACK_URL quando definida
   withCredentials: true,   // ESSENCIAL p/ JSESSIONID
-  timeout: 20000,
+  timeout: 30000,
 });
 export default api;
